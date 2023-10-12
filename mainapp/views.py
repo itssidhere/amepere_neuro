391ef70bfbba866cb272776387466c6b305b245a
sid <sidjha0001@gmail.com>
2023-10-12 15:57:09 +0200

    all models are now loading

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+import os
+from pathlib import Path
 
 
 def index(request):
-    return render(request, "index.html")
+    BASE_DIR = Path(__file__).resolve().parent
+    STL_DIR = BASE_DIR / "static" / "STLs"
+    stl_files = [f for f in os.listdir(STL_DIR) if f.endswith(".stl")]
+    return render(request, "index.html", {"stl_files": stl_files})
