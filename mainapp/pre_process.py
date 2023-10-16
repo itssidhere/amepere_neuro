748a6bb799a05ee57dbfddcdf64deae01df7231a
Gavin Gui <57789409+Gavin-Guiii@users.noreply.github.com>
2023-10-16 12:23:37 +0200

    Updates

--- mainapp/pre_process.py
+++ mainapp/pre_process.py
@@ -9,7 +9,7 @@ OUTPUT_FILE = MRI_FILE.replace(".nii.gz", "_synthseg.nii.gz")
 
 mri_seg.SegmentMRI(MRI_FILE, OUTPUT_FOLDER)
 
-Seg_Stl_PATH = "/Volumes/T7/INSA-LYON/amepere_neuro/mainapp/seg_stl.py"
+Seg_Stl_PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)), "seg_stl.py")
 
 # Run Slicer
 os.system(Slicer_PATH + " --no-splash --no-main-window --python-script " + Seg_Stl_PATH + " " + OUTPUT_FILE)
