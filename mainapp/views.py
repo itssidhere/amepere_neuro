841a0a6bae065e03350f6b0e8dd5141f5eff7056
Gavin Gui <57789409+Gavin-Guiii@users.noreply.github.com>
2023-10-18 13:28:20 +0200

    Update views.py

--- mainapp/views.py
+++ mainapp/views.py
@@ -8,9 +8,6 @@ import json
 
 
 def index(request):
-    # BASE_DIR = Path(__file__).resolve().parent
-    # STL_DIR = BASE_DIR / "static" / "STLs"
-    # stl_files = [f for f in os.listdir(STL_DIR) if f.endswith(".stl")]
     model_names = MriFile.objects.all().values_list('name', flat=True)
     return render(request, "index.html", {'model_names' :  model_names})
 
